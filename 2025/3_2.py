815c2f5d938c257e2c24e12a7028e146ba9a04c4
Rusty <rusty3925@gmail.com>
2025-12-04 07:15:52 -0800

    2025 day 3 part 2

---
+++ 2025/3_2.py
@@ -0,0 +1,28 @@
+import os
+
+os.chdir(os.path.dirname(__file__))
+
+banks = tuple(
+    tuple(
+        int(x) for x in bank
+    )
+    for bank in open("3.in").read().split("\n")
+)
+
+res = 0
+
+def maxNum(digits, k):
+    n = len(digits)
+    start = 0
+    res = 0
+    for i in range(k):
+        res *= 10
+        this = max(enumerate(digits[start:n-(k-i)+1]), key=lambda x: x[1])
+        start = this[0] + start + 1
+        res += this[1]
+    return res
+
+for bank in banks:
+    res += maxNum(bank, 12)
+
+print(res)
