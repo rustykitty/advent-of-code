e9dd2be688940b3578090bf9bd906201b6ebf666
Rusty <rusty3925@gmail.com>
2025-12-05 19:28:54 -0800

    2025 day 5 part 2

--- 2025/5_2.cpp
+++ 2025/5_2.cpp
@@ -2,49 +2,63 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <list>
+#include <algorithm>
+#include <utility>
+
 #include <cstdint>
 #include <cstdio>
 
 using namespace std;
 
 using u64 = uint64_t;
+using u64pair = pair<u64, u64>;
+
+vector<u64pair> merge_overlapping_ranges(vector<u64pair> ranges) {
+    sort(ranges.begin(), ranges.end());
+    auto it1 = ranges.begin();
+    while (it1 != ranges.end() && it1 + 1 != ranges.end()) {
+        auto& pa = *it1;
+        auto& pb = *(it1 + 1);
+        if (pa.second < pb.first) {
+            ++it1;
+            continue;
+        }
+        if (pa.first == pb.first || pa.second >= pb.first) {
+            pa.second = std::max(pa.second, pb.second);
+            ranges.erase(it1 + 1);
+            // pb is no longer valid here
+        }
+    }
+    return ranges;
+}
 
 inline bool inclusive_in_range(u64 lo, u64 hi, u64 n) {
     return lo <= n && n <= hi;
 }
 
-inline bool inclusive_in_range(const pair<u64, u64> range, u64 n) {
+inline bool inclusive_in_range(const u64pair range, u64 n) {
     return inclusive_in_range(range.first, range.second, n);
 }
 
 int main() {
-    std::vector<pair<u64, u64> > fresh;
+    std::vector<u64pair> fresh;
 
-    u64 lo, hi;
-
-    // scope
     {
         FILE* fin = fopen("5.in", "r");
         u64 x, y;
-        while (fscanf(fin, "%lu-%lu\n", &x, &y) > 0) {
+        while (fscanf(fin, "%lu-%lu\n", &x, &y) == 2) {
             fresh.push_back({ x, y });
-            lo = min(x, lo);
-            hi = max(y, hi);
         }
         fclose(fin);
     }
 
+    fresh = merge_overlapping_ranges(fresh);
+
     u64 res = 0;
 
-    for (u64 i = lo; i != hi + 1; ++i) {
-        for (const auto& p : fresh) {
-            if (inclusive_in_range(p, i)) {
-                ++res;
-                goto continue_outer;
-            }
-        }
-        // please don't kill me for the use of goto
-        continue_outer:;
+    for (const auto& p : fresh) {
+        res += p.second - p.first + 1;
     }
 
     cout << res << endl;
