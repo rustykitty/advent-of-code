72212766c913d798905214159cfb2ab324710e7a
Rusty <rusty3925@gmail.com>
2025-12-05 19:35:20 -0800

    cleanup and optimize

--- 2025/5_2.cpp
+++ 2025/5_2.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
 #include <list>
+#include <iterator>
 #include <algorithm>
-#include <utility>
 
 #include <cstdint>
 #include <cstdio>
@@ -14,35 +13,26 @@ using namespace std;
 using u64 = uint64_t;
 using u64pair = pair<u64, u64>;
 
-vector<u64pair> merge_overlapping_ranges(vector<u64pair> ranges) {
-    sort(ranges.begin(), ranges.end());
+void merge_overlapping_ranges(list<u64pair>& ranges) {
+    ranges.sort();
     auto it1 = ranges.begin();
-    while (it1 != ranges.end() && it1 + 1 != ranges.end()) {
+    while (it1 != ranges.end() && next(it1) != ranges.end()) {
         auto& pa = *it1;
-        auto& pb = *(it1 + 1);
+        auto& pb = *(next(it1));
         if (pa.second < pb.first) {
             ++it1;
             continue;
         }
         if (pa.first == pb.first || pa.second >= pb.first) {
-            pa.second = std::max(pa.second, pb.second);
-            ranges.erase(it1 + 1);
+            pa.second = max(pa.second, pb.second);
+            ranges.erase(next(it1));
             // pb is no longer valid here
         }
     }
-    return ranges;
-}
-
-inline bool inclusive_in_range(u64 lo, u64 hi, u64 n) {
-    return lo <= n && n <= hi;
-}
-
-inline bool inclusive_in_range(const u64pair range, u64 n) {
-    return inclusive_in_range(range.first, range.second, n);
 }
 
 int main() {
-    std::vector<u64pair> fresh;
+    list<u64pair> fresh;
 
     {
         FILE* fin = fopen("5.in", "r");
@@ -53,7 +43,7 @@ int main() {
         fclose(fin);
     }
 
-    fresh = merge_overlapping_ranges(fresh);
+    merge_overlapping_ranges(fresh);
 
     u64 res = 0;
 
