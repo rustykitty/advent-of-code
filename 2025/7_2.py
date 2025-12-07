7ff374788aba44add6bcdeaad8e6d5b1c8360ac9
Rusty <rusty3925@gmail.com>
2025-12-07 07:35:26 -0800

    check in a 2025 day 7 part 2 slow solution

---
+++ 2025/7_2.py
@@ -0,0 +1,17 @@
+data = open("7.in").read().rstrip().split("\n")
+s = data[0].index("S")
+n = len(data)
+m = len(data[0])
+
+def traverse(beam, pos):
+    if not (0 <= beam <= m):
+        return 0
+    if pos >= n:
+        return 1
+
+    if data[pos][beam] == "^":
+        return traverse(beam - 1, pos + 1) + traverse(beam + 1, pos + 1)
+    else:
+        return traverse(beam, pos + 1)
+
+print(traverse(s, 1))
